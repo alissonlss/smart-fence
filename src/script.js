4cbcf01f4319023b0a26f3903092977d59d43890
Alisson Leandro <alissonleo22@gmail.com>
2023-10-23 23:53:31 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,187 @@
+// Selecionando elementos HTML
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+const closeWebcam = document.getElementById('closeWebcam');
+const lineButton = document.getElementById('lineButton');
+
+// Função para verificar se o acesso à webcam é suportado
+function isUserMediaSupported() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+// Adicionar um evento de clique ao botão "Ativar Webcam"
+if (isUserMediaSupported()) {
+    enableWebcamButton.addEventListener('click', enableWebcam);
+} else {
+    console.warn('getUserMedia() não é suportado pelo seu navegador');
+}
+
+
+// Função para ativar a webcam e começar a classificação
+function enableWebcam(event) {
+    // Verificar se o modelo COCO-SSD foi carregado
+    if (!model) {
+        return;
+    }
+
+    // Ocultar o botão após o clique
+    event.target.classList.add('removed');
+
+    // Parâmetros para solicitar somente vídeo
+    const constraints = {
+        video: true
+    };
+
+    // Ativar o stream da webcam
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener('loadeddata', predictWebcam);
+    });
+
+    // Exibir o botão "Fechar Webcam" e o de ocultar linha
+    closeWebcam.classList.remove('removed');
+    lineButton.classList.remove('removed');
+
+    video.scrollIntoView({ behavior: "smooth" })
+}
+
+// Variável para armazenar o modelo
+var model = undefined;
+
+// Carregar o modelo COCO-SSD e mostrar a seção de demonstração quando estiver pronto
+function modelRun (){
+    cocoSsd.load().then(function (loadedModel) {
+        model = loadedModel;
+        demosSection.classList.remove('invisible');
+    });
+}
+
+modelRun();
+
+// Array para armazenar elementos HTML
+var children = [];
+
+// variável para armazenar a existencia da linha
+var lineExiste = false;
+
+
+// Função para classificar frames da webcam
+function predictWebcam() {
+    const class_mapping = ['person', 'dog', 'cat'];
+
+    model.detect(video).then(function (predictions) {
+        
+        for (let i = 0; i < children.length; i++) {
+            liveView.removeChild(children[i]);
+        }
+        children.splice(0);
+
+        const videoWidth = video.videoWidth;
+        const videoHeight = video.videoHeight;
+
+        const middleY = videoHeight / 2;
+
+        // Desenha uma linha horizontal no meio do quadro
+        if (!lineExiste){
+            const line = document.createElement('div');
+            line.id = "line";
+
+            line.style.position = 'absolute';
+            line.style.top = middleY + 'px';
+            line.style.left = '0';
+            line.style.width = videoWidth + 'px';
+            line.style.height = '2px'; // Espessura da linha
+            line.style.backgroundColor = 'red'; // Cor da linha
+        
+            liveView.appendChild(line);
+            lineExiste = true;
+        }
+
+        for (let n = 0; n < predictions.length; n++) {
+            if (predictions[n].score > 0.66 && class_mapping.includes(predictions[n].class)) {
+                const p = document.createElement('p');
+                p.id = "confidence";
+
+                p.innerText = predictions[n].class + ' - com ' +
+                    Math.round(parseFloat(predictions[n].score) * 100) +
+                    '% de confiança.';
+                p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: ' +
+                    (predictions[n].bbox[1] - 10) + 'px; width: ' +
+                    (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+                const highlighter = document.createElement('div');
+                highlighter.id = "frame";
+
+                highlighter.setAttribute('class', 'highlighter');
+                highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: ' +
+                    predictions[n].bbox[1] + 'px; width: ' +
+                    predictions[n].bbox[2] + 'px; height: ' +
+                    predictions[n].bbox[3] + 'px;';
+
+                liveView.appendChild(highlighter);
+                liveView.appendChild(p);
+                children.push(highlighter);
+                children.push(p);
+            }
+        }
+
+        window.requestAnimationFrame(predictWebcam);
+    });
+}
+
+
+// Event listener para o botão "Fechar Webcam"
+closeWebcam.addEventListener('click', () => {
+    const confidence = document.getElementById('confidence');
+    const frame = document.getElementById('frame');
+    const line = document.getElementById('line');
+
+    if (video.srcObject) {
+        const stream = video.srcObject;
+        const tracks = stream.getTracks();
+
+        tracks.forEach(track => track.stop());
+        video.srcObject = null;
+    }
+
+    // Ocultar o vídeo da visualização ao vivo
+    liveView.removeChild(confidence);
+    liveView.removeChild(frame);
+    liveView.removeChild(line);
+    
+    // Ocultar o botão "Fechar Webcam" e o de "Ocultar Linha"
+    closeWebcam.classList.add('removed');
+    lineButton.classList.add('removed');
+
+    // Exibir o botão "Ativar Webcam" novamente
+    enableWebcamButton.classList.remove('removed');
+
+
+    // reinicia as instancias
+    children = []
+    modelRun();
+    lineExiste = false;
+
+    // retorna ao todo
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+
+});
+
+lineButton.addEventListener("click", () => {
+    const textButton = {"Ocultar Linha": "Mostrar Linha", "Mostrar Linha": "Ocultar Linha"};
+    const line = document.getElementById('line');
+    const text = lineButton.textContent;
+
+    if (text === "Ocultar Linha"){
+        line.classList.add("removed");
+    } else {
+        line.classList.remove("removed");
+    }
+
+    lineButton.innerText = textButton[text];
+});
